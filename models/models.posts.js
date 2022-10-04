e9a8c0ebdd4be126f38d98daca4e99c2f3002782
boomkick <tranmanhthang107@gmail.com>
2022-10-04 20:44:11 +0700

    update post

--- models/models.posts.js
+++ models/models.posts.js
@@ -1,24 +1,24 @@
 var posts = [{
     id: 0,
-    title: 'Writing Challenge',
-    content: `Another writing challenge can be to take the individual sentences in the random paragraph and incorporate a single sentence from that into a new paragraph to create a short story. Unlike the random sentence generator, the sentences from the random paragraph will have some connection to one another so it will be a bit different. You also won't know exactly how many sentences will appear in the random paragraph.`,
+    title: 'Employee badge',
+    content: `To recieve a new employee badge, please fill out the form. Write in pen only. Please PRINT clearly.`,
     comments: [{
-        user: 'jessie',
-        comment: 'very good'
+        user: 'ceritna',
+        comment: 'i got it'
     }, {
-        user: 'henry',
-        comment: 'excellent'
+        user: 'asta',
+        comment: 'where could i find them ?'
     }]
 }, {
     id: 1,
-    title: 'Creative Writing',
-    content: 'Generating random paragraphs can be an excellent way for writers to get their creative flow going at the beginning of the day. The writer has no idea what topic the random paragraph will be about when it appears. This forces the writer to use creativity to complete one of three common writing challenges. The writer can use the paragraph as the first one of a short story and build upon it. A second option is to use the random paragraph somewhere in a short story they create. The third option is to have the random paragraph be the ending paragraph in a short story. No matter which of these challenges is undertaken, the writer is forced to use creativity to incorporate the paragraph',
+    title: 'Vacancy',
+    content: `Currently we have vacancies for the positions of waistaff. Duties include taking customer's orders, serving food and berverages, preparing itemized bills and accepting payments. Experience is preferred but not required. If you're interesting, call me at 555-888 to get more informations.`,
     comments: [{
-        user: 'jack',
-        comment: 'extremely good'
+        user: 'lindstrom',
+        comment: 'is there any left?'
     }, {
-        user: 'paul',
-        comment: 'bravo'
+        user: 'boomkick',
+        comment: `i can't connect to you`
     }]
 }]
 
